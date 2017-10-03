70512020209d24dab3704b4458f5b194a3fd86e7
Unknown <cef363@nyu.edu>
2017-10-03 08:41:06 -0400

    Added Movement? with problems
    the player can rotate but cant move around and i don't know why?

---
+++ Assets/scripts/MouseLook.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+	float mouseSensitivity = 100f;
+	float verticalLook = 0f; //remember our vertical mouse look eulerAngle (X axis)
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float mouseX = Input.GetAxis ("Mouse X"); //horizontal mouse velocity
+		float mouseY = Input.GetAxis ("Mouse Y"); //vertical mouse velocity
+
+		//basic mouse-look rotations
+		//transform.Rotate (-mouseY, 0f, 0f); //up/down only rotates camera
+		transform.parent.Rotate(0f, mouseX * Time.deltaTime * 500f, 0f);//left/right mouse rotates entire player
+
+		//more complex mouse-look vertical rotation where we CLAMP it
+		verticalLook -= mouseY * Time.deltaTime * mouseSensitivity;
+		verticalLook = Mathf.Clamp (verticalLook, -85f, 85f); //don't let player look straight up/down
+		//correction pass to unroll the camera? manually override Z eulerAngle to 0
+		transform.localEulerAngles = new Vector3(
+			verticalLook, //override X degrees with out own verticalLook value
+			transform.localEulerAngles.y,//put y back into itself
+			0f);//override the z to always be 0
+
+		//if user clicks in this game window then lock the mouse cursor
+		if (Input.GetMouseButtonDown(0)){ //0 = left-click, 1 = right-click, 2 = middle-click
+			Cursor.visible = false; //hide the mouse cursor
+			Cursor.lockState = CursorLockMode.Locked; //lock the mouse cursor to the center of the window
+		}
+	}
+}
---
+++ Assets/scripts/RigidbodyMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//put this script on a cube
+public class RigidbodyMove : MonoBehaviour {
+	Rigidbody myRigidbody;
+	Vector3 inputVector; //gets input from Update and sends it to physics
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody> (); //assign RB reference at Start
+	}
+
+	// Update is called once per "normal" frame
+	void Update () {
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		//get vertical input (W/S, up/down)
+		float verticalInput = Input.GetAxis("Vertical");
+
+		transform.Rotate (0f, horizontalInput * Time.deltaTime * 90f, 0f);
+		//put out input data into our "inputVector"
+		inputVector = new Vector3(0f, 0f, verticalInput);
+
+		//is the player moving faster than "1"? if so, normalize it
+		if (inputVector.magnitude > 1f) {
+			//prevents "faster when moving diagonally exploit
+			inputVector = Vector3.Normalize (inputVector);
+		}
+	}
+	//runs at a fixed timestep which is when physics runs
+	void FixedUpdate(){
+		//myRigidbody.AddForce (transform.TransformDirection(inputVector) * 25f); //use for things like applying thrust
+		myRigidbody.AddRelativeForce(inputVector * 25f, ForceMode.Force);  //BOTH THESE LINES OF CODE DO THE SAME THING
+
+		Debug.Log("my velocity: " + myRigidbody.velocity.ToString());
+		Debug.Log ("my speed " + myRigidbody.velocity.magnitude.ToString ());
+	}
+}
